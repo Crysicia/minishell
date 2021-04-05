c366e5a25ce6e2aca4caa6a5d4178875706d97bd
piecharton <pcharton@student.42.fr>
2021-04-05 22:47:11 +0200

    another try

--- tests/fct_lexer_test.c
+++ tests/fct_lexer_test.c
@@ -1,6 +1,9 @@
 #include <criterion/criterion.h>
 #include "../includes/header.h"
 
-Test_lexer_fct(example_suite, example_test) {
-    cr_expect(((lexer(NULL)) == -1), "Expect lexer to raise error if line is NULL");
+Test_lexer_fct(header_suite, header_name) {
+    cr_expect((, "Expect lexer to raise error if line is NULL");
+    cr_assert((lexer(NULL)) == -1));
+    (void)header_name;
+    (void)header_suite;
 }
