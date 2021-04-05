cfa7d822e17754e9a8861a19eb32e4b90c39ebac
piecharton <pcharton@student.42.fr>
2021-04-05 22:35:12 +0200

    Update fct_lexer_test.c

--- tests/fct_lexer_test.c
+++ tests/fct_lexer_test.c
@@ -1,6 +1,6 @@
 #include <criterion/criterion.h>
 #include "../includes/header.h"
 
-Test_lexer_fct(sample, test) {
-    cr_expect(lexer(NULL) == -1, "Expect lexer to raise error if line is NULL");
+Test_lexer_fct(example_suite, example_test) {
+    cr_expect(((lexer(NULL)) == -1), "Expect lexer to raise error if line is NULL");
 }
