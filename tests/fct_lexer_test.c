f2d54aa1040ad1a575fc5a234237115e2c761d77
piecharton <pcharton@student.42.fr>
2021-04-05 22:50:55 +0200

    dq  dq

--- tests/fct_lexer_test.c
+++ tests/fct_lexer_test.c
@@ -1,8 +1,8 @@
 #include <criterion/criterion.h>
 #include "../includes/header.h"
 
-Test_lexer_fct(header_suite, header_test) {
-    cr_assert((lexer(NULL)) == -1));
+Test(header_suite, header_test) {
+    cr_assert((lexer(NULL)) == -1);
     (void)header_test;
     (void)header_suite;
 }
