63a8bd172b3d51581629a5cb6db727a5206741bb
piecharton <pcharton@student.42.fr>
2021-04-06 23:29:12 +0200

    Revert "Revert "Weird commit""
    This reverts commit 2fb1eb67c4d52ec81f617134fd6338c2b0cfb3c3.

--- srcs/grammar.c
+++ srcs/grammar.c
@@ -1,32 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   grammar.h                                          :+:      :+:    :+:   */
+/*   grammar.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pcharton <pcharton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/06 22:11:36 by pcharton          #+#    #+#             */
-/*   Updated: 2021/04/06 22:11:36 by pcharton         ###   ########.fr       */
+/*   Created: 2021/04/06 22:56:28 by pcharton          #+#    #+#             */
+/*   Updated: 2021/04/06 22:56:28 by pcharton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GRAMMAR_H
-# define GRAMMAR_H
+#include "grammar.h"
 
-enum e_role
+int ft_strcmp(char *dst, char *src)
 {
-    role_not_given = 0,
-    command = 1,
-    argument = 2,
-    redirection = 3,
-    control_operator = 4
-};
-
-typedef struct  s_word
-{
-    char        *token;
-    enum e_role role;
-}               t_word;
-
-int ft_strcmp(char *dst, char *src);
-#endif
+    while (*dst && *src && (*dst == *src))
+    {
+        src++;
+        dst++;
+    }
+    return (*dst - *src);
+}
