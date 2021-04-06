5eb6f2036893590feff26eaf0bf2a09823692739
piecharton <pcharton@student.42.fr>
2021-04-07 00:14:27 +0200

    Update grammar_tests.c

--- tests/grammar_tests.c
+++ tests/grammar_tests.c
@@ -8,11 +8,11 @@ Test(grammar_suite, is_a_command_suite)
     t_word env;
     t_word error;
 
-    echo->token = strdup("echo");
-    cr_expect((is_a_command(&echo) == 0) && (echo->role == command), "this test should return 0 and role be set to command");
-    env->token = strdup("env");
-    cr_expect((is_a_command(&env) == 0) && (env->role == command), "this test should return 0 and role be set to command");
-    env->token = strdup("blablabla");
+    echo.token = strdup("echo");
+    cr_expect((is_a_command(&echo) == 0) && (echo.role == command), "this test should return 0 and role be set to command");
+    env.token = strdup("env");
+    cr_expect((is_a_command(&env) == 0) && (env.role == command), "this test should return 0 and role be set to command");
+    env.token = strdup("blablabla");
     cr_expect(is_a_command(&env) == 0, "this test should return -1, word is an error");
-    
+
 }
