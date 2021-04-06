8b1f902b13ddee75d05c7026e4f62f2db3d46252
piecharton <pcharton@student.42.fr>
2021-04-07 00:18:18 +0200

    Corrected test typo

--- tests/grammar_tests.c
+++ tests/grammar_tests.c
@@ -12,7 +12,7 @@ Test(grammar_suite, is_a_command_suite)
     cr_expect((is_a_command(&echo) == 0) && (echo.role == command), "this test should return 0 and role be set to command");
     env.token = strdup("env");
     cr_expect((is_a_command(&env) == 0) && (env.role == command), "this test should return 0 and role be set to command");
-    env.token = strdup("blablabla");
-    cr_expect(is_a_command(&env) == 0, "this test should return -1, word is an error");
-
+    error.token = strdup("blablabla");
+    cr_expect(is_a_command(&error) == -1, "this test should return -1, word is an error");
+//should had a test for NULL string
 }
