5e2bb4f41835781182e4c099576f0675de7777e7
piecharton <pcharton@student.42.fr>
2021-04-06 22:53:40 +0200

    Structure definition
    A role is given for each token
    The role is given inside a structure holding token and string

---
+++ includes/grammar.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   grammar.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcharton <pcharton@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/06 22:11:36 by pcharton          #+#    #+#             */
+/*   Updated: 2021/04/06 22:11:36 by pcharton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GRAMMAR_H
+# define GRAMMAR_H
+
+enum e_role
+{
+    role_not_given = 0;
+    command = 1;
+    argument = 2;
+    redirection = 3;
+    control_operator = 4;
+};
+
+typedef struct  s_word
+{
+    char        *token;
+    e_role      role;
+}               t_word;
+
+int ft_strcmp(char *dst, char *src);
+#endif
