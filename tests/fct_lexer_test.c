def7d7f79f624b643abaaf94d3aa11c8c80bf6b5
piecharton <pcharton@student.42.fr>
2021-04-06 08:24:45 +0200

    Added a test for get_word fct
    Test simply sends `echo bonjour` as an argument to get_word.
    fct is called twice and expected to return `echo` ten `bonjour`

--- tests/fct_lexer_test.c
+++ tests/fct_lexer_test.c
@@ -1,6 +1,23 @@
 #include <criterion/criterion.h>
 #include "../includes/header.h"
+#include <string.h>
 
 Test(header_suite, header_test) {
     cr_assert((lexer(NULL)) == -1);
+}
+
+/* Test get word */
+
+Test(header_suite, header_test) {
+    char *result;
+    char *input;
+    char *save;
+
+    input = "echo bonjour";
+    save = input;
+    result = get_word(&input);
+    cr_expect(strcmp(result, "echo") == 0, "Lexing echo bonjour did not return expected command");
+    free(result);
+    result = get_word(&input);
+    cr_expect(strcmp(result, "bonjour") == 0, "Lexing echo bonjour did not return expected argument")
 }
