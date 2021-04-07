78f475d8c6f767002ed2ed3cf828bbdd157bf26a
piecharton <46318475+piecharton@users.noreply.github.com>
2021-04-07 17:36:28 +0200

    Correction of wrong tabulation

--- srcs/grammar.c
+++ srcs/grammar.c
@@ -14,26 +14,26 @@
 
 int ft_strcmp(char *dst, char *src)
 {
-    while (*dst && *src && (*dst == *src))
-    {
-        src++;
-        dst++;
-    }
-    return (*dst - *src);
+	while (*dst && *src && (*dst == *src))
+	{
+		src++;
+		dst++;
+	}
+	return (*dst - *src);
 }
 
 int is_a_command(t_word *word)
 {
-    if (ft_strcmp(word->token, "echo") == 0)
-    {
-        word->role = command;
-        return (0);
-    }
-    else if (ft_strcmp(word->token, "env") == 0)
-    {
-        word->role = command;
-        return (0);
-    }
-    else
-        return (-1);
+	if (ft_strcmp(word->token, "echo") == 0)
+	{
+		word->role = command;
+		return (0);
+	}
+	else if (ft_strcmp(word->token, "env") == 0)
+	{
+		word->role = command;
+		return (0);
+	}
+	else
+		return (-1);
 }
