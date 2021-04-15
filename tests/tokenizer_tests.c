6bc049ab7487262fb0057cb1443fa42d73c81ebc
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-04-15 12:20:51 +0200

    Fix tokenizer tests

--- tests/tokenizer_tests.c
+++ tests/tokenizer_tests.c
@@ -38,7 +38,7 @@ Test(token_cutting_suite, token_cutting_echo_test)
 {
 	char *input[] = { "echo", "   echo  ", "echo  ", "   echo"};
 	for (int i = 0; ((input + i) && *(input + i)); i++)
-		cr_expect(strcmp(cut_token_string(input[i]), "echo") == 0),
+		cr_expect(strcmp(cut_token_string(input[i]), "echo") == 0,
 			"Please work your code to fit this condition");
 }
 
@@ -46,23 +46,23 @@ Test(token_cutting_suite, token_cutting_semi_colon_test)
 {
 	char *input[] = { ";", "   ;  ", ";  ", "   ;"};
 	for (int i = 0; ((input + i) && *(input + i)); i++)
-		cr_expect(strcmp(cut_token_string(input[i]), ";") == 0),
-			"Please work your code to fit this condition");
-}
-
-Test(token_cutting_suite, token_cutting_pipe_test)
-{
-	char *input[] = { ">", "   >  ", ">  ", "   >"};
-	for (int i = 0; ((input + i) && *(input + i)); i++)
-		cr_expect(strcmp(cut_token_string(input[i]), ">") == 0),
+		cr_expect(strcmp(cut_token_string(input[i]), ";") == 0,
 			"Please work your code to fit this condition");
 }
 
 Test(token_cutting_suite, token_cutting_chevronD_test)
 {
+	char *input[] = { ">", "   >  ", ">  ", "   >"};
+	for (int i = 0; ((input + i) && *(input + i)); i++)
+		cr_expect(strcmp(cut_token_string(input[i]), ">") == 0,
+			"Please work your code to fit this condition");
+}
+
+Test(token_cutting_suite, token_cutting_DchevronD_test)
+{
 	char *input[] = { ">>", "   >>  ", ">>  ", "   >>" , " >>file"};
 	for (int i = 0; ((input + i) && *(input + i)); i++)
-		cr_expect(strcmp(cut_token_string(input[i]), ">>") == 0),
+		cr_expect(strcmp(cut_token_string(input[i]), ">>") == 0,
 			"Please work your code to fit this condition");
 }
 
@@ -70,15 +70,15 @@ Test(token_cutting_suite, token_cutting_chevronG_test)
 {
 	char *input[] = { "<", "   <  ", "<  ", "   <"};
 	for (int i = 0; ((input + i) && *(input + i)); i++)
-		cr_expect(strcmp(cut_token_string(input[i]), "<") == 0),
+		cr_expect(strcmp(cut_token_string(input[i]), "<") == 0,
 			"Please work your code to fit this condition");
 }
 
-Test(token_cutting_suite, token_cutting_chevronG_test)
+Test(token_cutting_suite, token_cutting_pipe_test)
 {
 	char *input[] = { "|", "   |  ", "|  ", "   |"};
 	for (int i = 0; ((input + i) && *(input + i)); i++)
-		cr_expect(strcmp(cut_token_string(input[i]), "|") == 0),
+		cr_expect(strcmp(cut_token_string(input[i]), "|") == 0,
 			"Please work your code to fit this condition");
 }
 
@@ -86,6 +86,6 @@ Test(token_cutting_suite, token_cutting_backslash_test)
 {
 	char *input[] = { "\\ ", "   \\   ", "\\   ", "   \\ "};
 	for (int i = 0; ((input + i) && *(input + i)); i++)
-		cr_expect(strcmp(cut_token_string(input[i]), " ") == 0),
+		cr_expect(strcmp(cut_token_string(input[i]), " ") == 0,
 			"Please work your code to fit this condition");
 }
