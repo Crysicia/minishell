a6a3fa260f8e90bb78c05a1496f87a0cd5d1f77b
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-04-26 14:31:06 +0200

    Fix norm on path_utils

--- srcs/path_utils.c
+++ srcs/path_utils.c
@@ -6,20 +6,20 @@
 /*   By: lpassera <lpassera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/26 10:09:55 by lpassera          #+#    #+#             */
-/*   Updated: 2021/04/26 14:25:55 by lpassera         ###   ########.fr       */
+/*   Updated: 2021/04/26 14:30:42 by lpassera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/header.h"
 
-bool is_absolute_path(char *path)
+bool	is_absolute_path(char *path)
 {
 	if (!path)
 		return (false);
 	return (*path == '/');
 }
 
-bool is_path(char *path)
+bool	is_path(char *path)
 {
 	if (!path)
 		return (false);
