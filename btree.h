f25634345fba42c4282146793b9dbd00cf2e3c33
pcharton <pcharton@student.42.fr>
2021-04-27 22:52:41 +0200

    fix norm

--- btree.h
+++ btree.h
@@ -6,25 +6,25 @@
 /*   By: pcharton <pcharton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 21:45:36 by pcharton          #+#    #+#             */
-/*   Updated: 2021/04/27 22:32:08 by pcharton         ###   ########.fr       */
+/*   Updated: 2021/04/27 22:50:38 by pcharton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BTREE_H
-#define BTREE_H
+# define BTREE_H
 
-#include <stdlib.h>
+# include <stdlib.h>
 
-typedef struct      s_btree
+typedef struct s_btree
 {
-    struct  s_btree *left;
-    struct  s_btree *right;
-    void            *item;
-}                   t_btree;
+	struct s_btree	*left;
+	struct s_btree	*right;
+	void			*item;
+}					t_btree;
 
-t_btree *btree_create_node(void *item);
-void    btree_apply_prefix(t_btree *root, void (*applyf)(void *));
-void    btree_apply_infix(t_btree *root, void (*applyf)(void *));
-void    btree_apply_suffix(t_btree *root, void (*applyf)(void *));
+t_btree	*btree_create_node(void *item);
+void	btree_apply_prefix(t_btree *root, void (*applyf)(void *));
+void	btree_apply_infix(t_btree *root, void (*applyf)(void *));
+void	btree_apply_suffix(t_btree *root, void (*applyf)(void *));
 
 #endif
