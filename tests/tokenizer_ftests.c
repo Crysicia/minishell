397502247dddf6823429c11713e1d05b99ec970b
piecharton <pcharton@student.42.fr>
2021-04-30 11:41:07 +0200

    Add first functionnal test for get next token
    "'ec'\"ho\" bonjour ;"

--- tests/tokenizer_ftests.c
+++ tests/tokenizer_ftests.c
@@ -33,3 +33,28 @@ ParameterizedTestParameters(tokenizer_behaviour, simple_quote_test)
 								sizeof(tests)/sizeof(t_tok_input_test)));
 }
 */
+
+Test(tokenizer_loop_suite, testing_normal_command)
+{
+    char        buffer[] = "'ec'\"ho\" bonjour ;";
+    char        *expected_token_str[] = { "'ec'\"ho\"", "bonjour", ";"};
+    t_tok_type expected_token_type[] = { word, word, operator};
+    int         nb_of_tokens = 3;
+    size_t len  = strlen(buffer);
+
+    t_token     *token;
+    char        *tmp = buffer;
+    for (int i = 0; i < nb_of_tokens; i++)
+    {
+        token = get_next_token(tmp);
+        cr_expect_str_eq(expected_token_str[i], token->cmd,
+            "Test get_next_token on given command\n"
+            "Input : [%s], Expected : [%s], Got : [%s]\n",
+            buffer, expected_token_str[i], token->cmd);
+        cr_assert_eq(expected_token_type[i], token->role);
+        tmp = strstr(buffer, token->cmd);
+        tmp += strlen(token->cmd);
+        if (tmp >= buffer + len)
+            break;
+    }
+}
