23726d113bef0a6798ba1035fb312d64ca124c0e
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-05-14 15:27:10 +0200

    Add underscore tois_env_valid

--- srcs/builtin_utils.c
+++ srcs/builtin_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: crysicia <crysicia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lpassera <lpassera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 12:31:24 by lpassera          #+#    #+#             */
-/*   Updated: 2021/04/28 14:04:54 by crysicia         ###   ########.fr       */
+/*   Updated: 2021/05/14 15:24:36 by lpassera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ bool	is_env_valid(char *env, bool can_contain_eq)
 	i = 0;
 	if (ft_isdigit(env[0]))
 		return (false);
-	while (ft_isalnum(env[i]) && env[i] != '=')
+	while ((ft_isalnum(env[i]) || env[i] == '_') && env[i] != '=')
 		i++;
 	if (i > 0 && (!env[i] || ft_isspace(env[i])
 			|| (env[i] == '=' && can_contain_eq)))
