5212d82656454c2aa4e4ce4f918a720ba2fb0f60
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-05-20 17:02:31 +0200

    WIP - could not work

--- srcs/pipeline.c
+++ srcs/pipeline.c
@@ -6,40 +6,47 @@
 /*   By: lpassera <lpassera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 13:18:19 by lpassera          #+#    #+#             */
-/*   Updated: 2021/05/19 17:00:15 by lpassera         ###   ########.fr       */
+/*   Updated: 2021/05/20 16:50:06 by lpassera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/header.h"
 
-int get_pipeline_placement(t_list *commands)
+int get_pipeline_placement(int current, int total)
 {
-	(void)commands;
-	return (NOT_IN_PIPELINE);
+	if (current == 0)
+		return (FIRST_IN_PIPELINE);
+	if (current == total - 1)
+		return (IN_PIPELINE);
+	return (LAST_IN_PIPELINE);
 }
 
 int execute_pipeline(t_pipeline *pipeline)
 {
 	t_pipes	pipes;
+	int count;
 
+	count = pipeline->pipe_count;
 	if (!create_pipes(&pipes))
 		return (ERR_PIPE_FAILED);
-	execute_pipeline_loop(&pipes, pipeline->commands);
+	execute_pipeline_loop(&pipes, pipeline->commands, count);
 	close_pipes(&pipes);
 	return (0);
 }
 
-int execute_pipeline_loop(t_pipes *pipes, t_list *commands)
+int execute_pipeline_loop(t_pipes *pipes, t_list *commands, int pipe_count)
 {
 	int pid;
 	char **command;
+	int current;
 
+	current = 0;
 	while (commands)
 	{
 		command = command_format(commands);
 		if (!command)
 			return (ERR_MALLOC_FAILED);
-		pid = execute_pipe(command, pipes, get_pipeline_placement(commands));
+		pid = execute_pipe(command, pipes, get_pipeline_placement(current, pipe_count));
 		if (pid == -ERR_FORK_FAILED)
 			printf("ERROR: Could not create child process.\n");
 		else if (pid > 0)
@@ -50,6 +57,7 @@ int execute_pipeline_loop(t_pipes *pipes, t_list *commands)
 			g_globals->current_pid = 0;
 		}
 		commands = commands->next;
+		current++;
 	}
 	return (0);
 }
