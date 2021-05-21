5bd65137027ff2dc0c0bcdaf6a1df84d6339d4e5
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-05-21 11:13:14 +0200

    Working pipes

--- srcs/pipeline.c
+++ srcs/pipeline.c
@@ -6,7 +6,7 @@
 /*   By: lpassera <lpassera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 13:18:19 by lpassera          #+#    #+#             */
-/*   Updated: 2021/05/21 07:53:14 by lpassera         ###   ########.fr       */
+/*   Updated: 2021/05/21 11:12:53 by lpassera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int get_pipeline_placement(int current, int total)
 {
 	if (current == 0)
 		return (FIRST_IN_PIPELINE);
-	if (current == (total))
+	if (current == (total - 1))
 		return (LAST_IN_PIPELINE);
 	return (IN_PIPELINE);
 }
@@ -26,7 +26,6 @@ int execute_pipeline(t_pipeline *pipeline)
 	t_pipes	pipes;
 	int count;
 
-	printf("EXECUTE PIPELINE PIPES\n");
 	count = pipeline->pipe_count;
 	if (!create_pipes(&pipes))
 		return (ERR_PIPE_FAILED);
@@ -39,11 +38,15 @@ int execute_pipeline_loop(t_pipes *pipes, t_list *commands, int pipe_count)
 {
 	int pid;
 	int current;
+	int placement;
 
 	current = 0;
 	while (commands)
 	{
-		pid = execute_pipe(commands->content, pipes, get_pipeline_placement(current, pipe_count));
+		placement = get_pipeline_placement(current, pipe_count);
+		if (placement != FIRST_IN_PIPELINE)
+			swap_pipes(pipes);
+		pid = execute_pipe(commands->content, pipes, placement);
 		if (pid == -ERR_FORK_FAILED)
 			printf("ERROR: Could not create child process.\n");
 		else if (pid > 0)
@@ -69,7 +72,6 @@ int	execute_pipe(t_simple_command *command, t_pipes *pipes, int command_flag)
 	if (pid < 0)
 		return (-ERR_FORK_FAILED);
 	arguments = command_format(command->words);
-	swap_pipes(pipes);
 	if (!arguments)
 		return (ERR_MALLOC_FAILED);
 	else if (pid == 0)
@@ -83,8 +85,8 @@ int	execute_pipe(t_simple_command *command, t_pipes *pipes, int command_flag)
 		if (!path)
 			return (-1);
 		execve(path, arguments, list_to_array(g_globals->env));
+		exit(0);
 	}
 	close_relevant_pipes(pipes, command_flag);
-	printf("PARENT CLOSD PIPES\n");
 	return (pid);
 }
