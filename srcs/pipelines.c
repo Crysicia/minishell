6e44247cede4f48d8a64ff6017ed6edd2a597eed
piecharton <pcharton@student.42.fr>
2021-07-05 18:38:21 +0200

    removing pipes_utils

--- srcs/pipelines.c
+++ srcs/pipelines.c
@@ -6,7 +6,7 @@
 /*   By: lpassera <lpassera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/15 14:40:09 by pcharton          #+#    #+#             */
-/*   Updated: 2021/07/05 16:27:41 by pcharton         ###   ########.fr       */
+/*   Updated: 2021/07/05 18:35:25 by pcharton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,4 +51,45 @@ bool	check_if_pipeline(t_simple_command	*command)
 	if (token->role == operator && !ft_strncmp(token->cmd, "|", 1))
 		return (1);
 	return (0);
+}
+
+int	**allocate_pipe_tab(int	nb)
+{
+	int	i;
+	int	**tab;
+
+	if (nb > 255)
+		return (NULL);
+	tab = malloc(sizeof(int *) * (nb + 1));
+	if (tab)
+	{
+		i = 0;
+		while (i < nb)
+		{
+			tab[i] = malloc(sizeof(int) * 2);
+			if (!tab[i])
+				return (deallocate_pipe_tab(tab, i));
+			i++;
+		}
+		tab[i] = NULL;
+	}
+	return (tab);
+}
+
+void	*deallocate_pipe_tab(int **tab, int nb)
+{
+	int	i;
+
+	i = 0;
+	while (i < nb)
+	{
+		close(tab[i][0]);
+		close(tab[i][1]);
+		free(tab[i]);
+		tab[i] = NULL;
+		i++;
+	}
+	free(tab);
+	tab = NULL;
+	return (NULL);
 }
